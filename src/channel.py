c68609afd484cd72e3c431892afff2d1269e37e8
Ulugbek Nishanov <daynnishanov@gmail.com>
2023-06-22 21:44:52 +0700

    add

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,13 @@
-
-
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+
+        """
+        Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API.
+        """
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(self.channel_id)
