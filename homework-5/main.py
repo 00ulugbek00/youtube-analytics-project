55baa858c01dd070531ee9127c0ccb8dab833746
Ulugbek Nishanov <daynnishanov@gmail.com>
2023-07-08 17:24:53 +0700

    add Реализуйте класс PlayList, который инициализируется id плейлиста и имеет следующие публичные атрибуты:
    название плейлиста
    ссылку на плейлист
    Реализуйте следующие методы класса PlayList
    
    total_duration возвращает объект класса datetime.timedelta с суммарной длительность плейлиста (обращение как к свойству, использовать @property)
    show_best_video() возвращает ссылку на самое популярное видео из плейлиста (по количеству лайков)

--- homework-5/main.py
+++ homework-5/main.py
@@ -1,15 +1,15 @@
 import datetime
 
-from src.playlist import PlayList
+from src.video import PlayList
 
 if __name__ == '__main__':
-    pl = PlayList('PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw')
-    assert pl.title == "Moscow Python Meetup №81"
-    assert pl.url == "https://www.youtube.com/playlist?list=PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw"
+    pl = PlayList('PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb')
+    assert pl.title_list == "Редакция. АнтиТревел"
+    assert pl.url_list == "https://www.youtube.com/playlist?list=PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb"
 
     duration = pl.total_duration
-    assert str(duration) == "1:49:52"
+    assert str(duration) == "3:41:01"
     assert isinstance(duration, datetime.timedelta)
-    assert duration.total_seconds() == 6592.0
+    assert duration.total_seconds() == 13261.0
 
-    assert pl.show_best_video() == "https://youtu.be/cUGyMzWQcGM"
+    assert pl.show_best_video() == "https://youtu.be/9Bv2zltQKQA"
